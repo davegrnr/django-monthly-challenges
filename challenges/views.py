e0bcb7f014d8615450481c080a4e044a66d1ed4b
davegrnr <davegrnr@gmail.com>
2021-08-13 13:14:44 -0400

    Add some routes and views

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def january(request):
+    return HttpResponse("Eat no meat for the entire month")
+
+
+def february(request):
+    return HttpResponse("Run 30 miles")
+
+
+def march(request):
+    return HttpResponse("Learn Django for 20 mins every day")
